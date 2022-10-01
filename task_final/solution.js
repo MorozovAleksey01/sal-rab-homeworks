6860dd7dbfe02c9ff0450503ad480b1cac96f940
MorozovAleksey01 <a.morozov@4doc.ru>
2022-10-01 12:38:06 +0300

    Task_Final

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,17 +38,19 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {client: '', order: {}, goods: []};
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+     //   data.goods.push(goods[i].title);
+     data.goods.title = goods[i].title;
+     data.goods.count = goods[i].count;
     }
 
-    data.order.address = address;
+    //data.order.address = address;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
     data.order.sum = sum;
 
     data.client = name + " " + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
